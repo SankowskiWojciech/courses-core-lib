38ae85b7f312645d09258cf5d8f99d78f52b24cb
WojciechS <sankowski.wojciech@gmail.com>
2020-10-17 21:39:39 +0200

    added LessonFileResponse model class

---
+++ src/main/java/com/github/sankowskiwojciech/coursescorelib/model/lesson/LessonFileResponse.java
@@ -0,0 +1,16 @@
+package com.github.sankowskiwojciech.coursescorelib.model.lesson;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class LessonFileResponse {
+    private long fileId;
+    private String name;
+    private String extension;
+    private String createdBy;
+    private LocalDateTime creationDateTime;
+}
