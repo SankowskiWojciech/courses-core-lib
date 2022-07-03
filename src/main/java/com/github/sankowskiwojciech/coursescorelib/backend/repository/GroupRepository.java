3b938cd3e6e51fa8b797af7c57b3e4b2a1076c3e
Wojciech Sankowsi <sankowski.wojciech@gmail.com>
2022-07-03 12:21:28 +0200

    added group related classes

--- src/main/java/com/github/sankowskiwojciech/coursescorelib/backend/repository/GroupRepository.java
+++ src/main/java/com/github/sankowskiwojciech/coursescorelib/backend/repository/GroupRepository.java
@@ -4,6 +4,9 @@ import com.github.sankowskiwojciech.coursescorelib.model.db.group.GroupEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GroupRepository extends JpaRepository<GroupEntity, String> {
+    List<GroupEntity> findAllByTutorEntityEmailAddressAndSubdomainEntitySubdomainId(String tutorEmailAddress, String subdomainAlias);
 }
---
+++ src/main/java/com/github/sankowskiwojciech/coursescorelib/model/group/GroupResponse.java
@@ -0,0 +1,11 @@
+package com.github.sankowskiwojciech.coursescorelib.model.group;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class GroupResponse {
+    private String id;
+    private String name;
+}
